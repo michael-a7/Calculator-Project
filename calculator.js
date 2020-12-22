fbf88b4b8d9109e212139cc1ee64b6b080fcbc60
michael-a7 <74381482+michael-a7@users.noreply.github.com>
2020-12-21 16:41:17 -0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,39 @@
+document.getElementById("output").value="0";
+
+let number = 
+function clearFunction() {
+    document.getElementById("output").innerHTML = "0";
+}
+function Insert0() {
+    document.getElementById("output").innerHTML = "0";
+}
+function Insert1() {
+    document.getElementById("output").innerHTML = "1";
+}
+function Insert2() {
+    document.getElementById("output").innerHTML = "2";
+}
+function Insert3() {
+    document.getElementById("output").innerHTML = "3";
+}
+function Insert4() {
+    document.getElementById("output").innerHTML = "4";
+}
+function Insert5() {
+    document.getElementById("output").innerHTML = "5";
+}
+function Insert6() {
+    document.getElementById("output").innerHTML = "6";
+}
+function Insert7() {
+    document.getElementById("output").innerHTML = "7";
+}
+function Insert8() {
+    document.getElementById("output").innerHTML = "8";
+}
+function Insert9() {
+    document.getElementById("output").innerHTML = "9";
+}
+function Insertdec() {
+    document.getElementById("output").innerHTML = ".";
+}
